9a29aa00bbc65807f1241c49cae282ddd7cf05f3
Criztiann <crisgalindoespina@gmail.com>
2022-11-17 22:07:05 -0500

    Crud vehiculos

--- src/main/java/com/example/demo/controller/PqrsController.java
+++ src/main/java/com/example/demo/controller/PqrsController.java
@@ -4,7 +4,6 @@ import com.example.demo.entity.Pqrs;
 import com.example.demo.entity.Usuario;
 import com.example.demo.repository.IPqrsRepository;
 import com.example.demo.repository.IUsuarioRepository;
-import com.sun.org.apache.xpath.internal.operations.Mod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
