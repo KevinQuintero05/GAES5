ef0a394f0953c2791ecb5f890638e96319e0e6b6
kevinQuintero05 <kevin.quintero06@misena.edu.co>
2022-12-05 21:19:38 -0500

    botones

--- src/main/java/com/example/demo/controller/AdminController.java
+++ src/main/java/com/example/demo/controller/AdminController.java
@@ -1,13 +1,26 @@
 package com.example.demo.controller;
 
+import com.example.demo.entity.Usuario;
+import com.example.demo.repository.IUsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.List;
+
 @Controller
 public class AdminController {
+
+    @Autowired
+    private IUsuarioRepository iUsuarioRepository;
     @RequestMapping(value = {"/admin/dashboard"}, method = RequestMethod.GET)
-    public String adminHome(){
+    public String adminHome(Model model){
+        List<Usuario> usuarioList = iUsuarioRepository.findAll();
+        model.addAttribute("usuarioList", usuarioList);
         return "admin/dashboard";
     }
+
 }
